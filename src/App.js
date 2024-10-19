6f90c4f011db7d6e1225409450709ca163b57d67
Dhruva Kumar <dhr7va@gmail.com>
2024-10-19 21:47:17 +0530

    added cart button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import ProductList from "./Components/ProductList";
 
 const App = () => {
   const [products, setProducts] = useState([]);
-
+  const [cart, setCart] = useState([]);
   const addProduct = (product) => {
     setProducts([...products, product]);
   }
+
+  const updateCart = (size, product) => {
+    let quantityKey = 'quantity' + size;
+    if (product[quantityKey] > 0) {
+      setCart([...cart, { ...product, size }]);
+      product[quantityKey]--;
+      const updatedProducts = [...products];
+      updatedProducts[products.indexOf(product)] = product;
+      setProducts(updatedProducts);
+    }
+  };
   return (
     <div>
       <h1>Tshirt Shop</h1>
       <AddProductsFort addProduct={addProduct} />
-      <ProductList products={products} />
+      <ProductList products={products} updateCart={updateCart} />
+      <button >Cart {cart.length}</button>
+
     </div>
   );
 }
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ProductItem from './ProductItem';
 
-const ProductList = ({ products }) => {
+const ProductList = ({ products, updateCart }) => {
     return (
         <div>
             {products.map((product, index) => (
-                <ProductItem key={index} product={product} />
+                <ProductItem key={index} product={product} updateCart={updateCart} />
             ))}
         </div>
     );
